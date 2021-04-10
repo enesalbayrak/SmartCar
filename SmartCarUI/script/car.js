a8ad693cf20a7d81ef147bf1aecaf01d2b8e3e4f
enesalbayrak <bm.bekir@gmail.com>
2021-04-10 20:22:30 +0300

    lights

--- SmartCarUI/script/car.js
+++ SmartCarUI/script/car.js
@@ -1,20 +1,197 @@
-/*var wait = async (time) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, time);
-  });
+function runAndSetInterval(func, time) {
+  func();
+  return setInterval(func, time);
+}
+var webStatus = {
+  backDistance: 0,
+  frontDistance: 0,
+  lightLevel: 40,
 };
 
-(async () => {
-  let x = 0;
-  while (true) {
-    await wait(1000);
-    console.log(++x);
-  }
-})();*/
+//#region  signal
+var signalValues = {
+  status: "CLOSE",
+  interval: -1,
+  last: false,
+};
+function setSignalLamp(status) {
+  clearInterval(signalValues.interval);
+  signalValues.last = false;
+  document.getElementById("signal-left-lamp").src = "imgs/signalclose.svg";
+  document.getElementById("signal-right-lamp").src = "imgs/signalclose.svg";
 
-let worker = new Worker("script/worker.js");
-worker.addEventListener("message", function (e) {
-  console.log(e.data);
+  if (status == "LEFT") {
+    signalValues.interval = runAndSetInterval(() => {
+      if (signalValues.last) {
+        signalValues.last = false;
+        document.getElementById("signal-left-lamp").src =
+          "imgs/signalclose.svg";
+        document.getElementById("signal-close-audio").play();
+      } else {
+        signalValues.last = true;
+        document.getElementById("signal-left-lamp").src = "imgs/signalopen.svg";
+        document.getElementById("signal-open-audio").play();
+      }
+    }, 500);
+  } else if (status == "RIGHT") {
+    signalValues.interval = runAndSetInterval(() => {
+      if (signalValues.last) {
+        signalValues.last = false;
+        document.getElementById("signal-right-lamp").src =
+          "imgs/signalclose.svg";
+        document.getElementById("signal-close-audio").play();
+      } else {
+        signalValues.last = true;
+        document.getElementById("signal-right-lamp").src =
+          "imgs/signalopen.svg";
+        document.getElementById("signal-open-audio").play();
+      }
+    }, 500);
+  } else if (status == "QUAD") {
+    signalValues.interval = runAndSetInterval(() => {
+      if (signalValues.last) {
+        signalValues.last = false;
+        document.getElementById("signal-left-lamp").src =
+          "imgs/signalclose.svg";
+        document.getElementById("signal-right-lamp").src =
+          "imgs/signalclose.svg";
+        document.getElementById("signal-close-audio").play();
+      } else {
+        signalValues.last = true;
+        document.getElementById("signal-left-lamp").src = "imgs/signalopen.svg";
+        document.getElementById("signal-right-lamp").src =
+          "imgs/signalopen.svg";
+        document.getElementById("signal-open-audio").play();
+      }
+    }, 500);
+  } else document.getElementById("signal-close-audio").play();
+}
+
+var signalStatus = {
+  quad: false,
+  signal: "CLOSE",
+};
+
+function setSignalStatus(status) {
+  if (status == "LEFT") {
+    signalStatus.signal = "LEFT";
+    document.getElementById("signal-left").classList.add("active");
+    document.getElementById("signal-right").classList.remove("active");
+  } else if (status == "RIGHT") {
+    signalStatus.signal = "RIGHT";
+    document.getElementById("signal-right").classList.add("active");
+    document.getElementById("signal-left").classList.remove("active");
+  } else if (status == "QUAD") {
+    signalStatus.quad = true;
+    document.getElementById("signal-quad").classList.add("active");
+  } else if (status == "QUADCLOSE") {
+    signalStatus.quad = false;
+    document.getElementById("signal-quad").classList.remove("active");
+  } else {
+    signalStatus.signal = "CLOSE";
+    document.getElementById("signal-left").classList.remove("active");
+    document.getElementById("signal-right").classList.remove("active");
+  }
+
+  setSignalLamp(signalStatus.quad ? "QUAD" : signalStatus.signal);
+}
+
+document.getElementById("signal-left").addEventListener("click", () => {
+  setSignalStatus(signalStatus.signal == "LEFT" ? "CLOSE" : "LEFT");
 });
+document.getElementById("signal-right").addEventListener("click", () => {
+  setSignalStatus(signalStatus.signal == "RIGHT" ? "CLOSE" : "RIGHT");
+});
+document.getElementById("signal-close").addEventListener("click", () => {
+  setSignalStatus("CLOSE");
+});
+document.getElementById("signal-quad").addEventListener("click", () => {
+  setSignalStatus(signalStatus.quad ? "QUADCLOSE" : "QUAD");
+});
+
+document.addEventListener("keydown", (ev) => {
+  switch (ev.key.toLowerCase()) {
+    case "e":
+      setSignalStatus(signalStatus.signal == "LEFT" ? "CLOSE" : "LEFT");
+      break;
+    case "r":
+      setSignalStatus(signalStatus.signal == "RIGHT" ? "CLOSE" : "RIGHT");
+      break;
+    case "t":
+      setSignalStatus("CLOSE");
+      break;
+
+    case "o":
+      setSignalStatus(signalStatus.quad ? "QUADCLOSE" : "QUAD");
+      break;
+    default:
+      break;
+  }
+});
+//#endregion
+
+//#region headlight
+var headlightStatus = {
+  status: "OFF",
+  active: false,
+  long: false,
+};
+function setHeadlightStyles() {
+  if (headlightStatus.long)
+    document.getElementById("headlight-long").classList.add("active");
+  else document.getElementById("headlight-long").classList.remove("active");
+
+  if (headlightStatus.long && headlightStatus.active)
+    document.getElementById("headlight-long-lamp").src =
+      "imgs/headlightLopen.svg";
+  else
+    document.getElementById("headlight-long-lamp").src =
+      "imgs/headlightLclose.svg";
+  if (headlightStatus.status == "ON") {
+    document.getElementById("headlight-on").classList.add("active");
+    document.getElementById("headlight-off").classList.remove("active");
+    document.getElementById("headlight-auto").classList.remove("active");
+  } else if (headlightStatus.status == "AUTO") {
+    document.getElementById("headlight-on").classList.remove("active");
+    document.getElementById("headlight-off").classList.remove("active");
+    document.getElementById("headlight-auto").classList.add("active");
+  } else {
+    document.getElementById("headlight-on").classList.remove("active");
+    document.getElementById("headlight-off").classList.add("active");
+    document.getElementById("headlight-auto").classList.remove("active");
+  }
+
+  if (headlightStatus.active)
+    document.getElementById("headlight-lamp").src = "imgs/headlightopen.svg";
+  else
+    document.getElementById("headlight-lamp").src = "imgs/headlightclose.svg";
+}
+function setLongHeadlight(status) {
+  headlightStatus.long = status;
+  setHeadlightStyles();
+}
+
+function setHeadlight(status) {
+  headlightStatus.status = status;
+  if (status == "ON") headlightStatus.active = true;
+  else if (status == "AUTO" && webStatus.lightLevel <= 30)
+    headlightStatus.active = true;
+  else headlightStatus.active = false;
+  setHeadlightStyles();
+}
+
+document.getElementById("headlight-long").addEventListener("click", () => {
+  setLongHeadlight(!headlightStatus.long);
+});
+
+document.getElementById("headlight-on").addEventListener("click", () => {
+  setHeadlight("ON");
+});
+document.getElementById("headlight-off").addEventListener("click", () => {
+  setHeadlight("OFF");
+});
+document.getElementById("headlight-auto").addEventListener("click", () => {
+  setHeadlight("AUTO");
+});
+
+//#endregion
